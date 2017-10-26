d851e244c2da97e7e8bed9b1a8eb9c50f2fd7e68
compsci <compsci@compsci>
2017-10-26 10:21:44 -0600

    Added Grading Requirements and JOPs

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -1,23 +1,36 @@
 package caesar;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class BeginHere {
 
 	public static void main(String[] args) {
-		Scanner kbd = new Scanner(System.in);
+		
+	  /*Object[] possibilities = {"encode", "decode", "crack", "exit"};
+		String s = (String)JOptionPane.showInputDialog(
+		                    null,
+		                    "Please make you selection",
+		                    "Start Here",
+		                    JOptionPane.PLAIN_MESSAGE,
+		                    possibilities,
+		                    "encode");
+	   */
+
+		//String msg = "Would you like to encode a plain text message?";
+		//int confirm = JOptionPane.showConfirmDialog(null, msg);
 		
 		CaesarCipher myCipher = new CaesarCipher();
 		
-		System.out.println("Please enter the plain text to encode.");
-		String plainText = kbd.nextLine().toLowerCase();
+		String msg = "Please enter the plain text to encode.";
+		String plainText = JOptionPane.showInputDialog(msg);
 		
-		System.out.println("Please enter the key.");
-		int key = kbd.nextInt();
+		msg="Please enter the key.";
+		int key = Integer.parseInt(JOptionPane.showInputDialog(msg));
 		//System.out.println("Your key is: " + key);
 		
 		String codedText = myCipher.encode(plainText, key);
-		System.out.println("Your secret message is: " + codedText);
+		msg="Your secret message is: " + codedText;
+		JOptionPane.showMessageDialog(null, msg);
 		
 	}
 
